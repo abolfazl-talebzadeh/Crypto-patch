b5122fdc606ec8c724da4e0862103078afbb586c
Abolfazl Talebzadeh <arts.archeryco@gmail.com>
2021-10-03 00:44:01 +0200

    "finishing Z*p"

--- code/cyclic_group.r
+++ code/cyclic_group.r
@@ -1,10 +1,12 @@
 rm(list=ls())
-x= seq(100)
+x= seq(10)
 a=(6*x-1)
 b=(6*x+1)
 c=c(2,3)
 d= c(2,3)
 f=sort(union(a,b)) 
-Prime=union(c,f)
-P=(Prime*2+1)
-P
+q=union(c,f)
+Psequence=(q*2+1)
+p=sample(Psequence,1)
+z_star_p=seq(1,p)
+z_star_p
