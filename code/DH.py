bf7c098c53dfbcaac8134c83c4d7a97c87c0b462
Abolfazl Talebzadeh <arts.archeryco@gmail.com>
2021-10-08 20:17:43 +0200

    The Final One

--- code/DH.py
+++ code/DH.py
@@ -3,15 +3,19 @@ class DH:
     primes=[]
     q_primes=[]
     p=0
-    y=0
+    ya=0
+    yb=0
+    k=0
     a=0
+    b=0
     def __init__(self, n,g):
         self.n=n
         #self.x=x
         self.g=g
         self.primes_gen()
-        self.p_creator()
+        #self.p_creator()
         self.q_assigner()
+        self.g_calculator()
         return
     ##############################
     def primes_gen(self):
@@ -27,21 +31,54 @@ class DH:
                 self.primes.append(i)
                 is_prime=False
     #################################
-    def p_creator(self):
-        self.q_primes=[(2*self.primes[i])+1 for i in range (0,len(self.primes)-1)]
+    """ def p_creator(self):
+        self.q_primes=[(2*self.primes[i])+1 for i in range (0,len(self.primes)-1)] """
     ####################################
     def q_assigner(self):
         pos=random.randint(0,len(self.primes)-1)
-        self.p=self.primes[pos]-1
+        self.p=self.primes[pos]
+    #####################################
+    def g_calculator(self):
+        generators=[]
+        z=[]
+        for x in range(1, self.p):
+            z.append(x)
+        for g in self.primes:
+            if g<self.p:
+                temp=[]
+                for y in range(1, self.p):
+                    temp.append((g**y)%self.p)
+                temp.sort()
+                temp_set=set(temp)
+                if temp_set==set(z):
+                    generators.append(g)
+        self.g=generators[random.randint(0,len(generators)-1)]
+        return generators
+    #####################################
+    def secret_key_generator(self):
+        
+        return
     #####################################
     def secret_number_generattor(self):
-        self.a=random.randint(1,self.p)
-        self.y=(self.g**self.a)%(self.p)
+        self.a=random.randint(1,self.p-1)
+        self.ya=(self.g**self.a)%(self.p)
+        self.b=random.randint(1,self.p-1)
+        self.yb=(self.g**self.b)%(self.p)
+        if (self.ya ** self.b) % (self.p) == (self.yb ** self.a) % (self.p):
+            self.k=(self.ya ** self.b) % (self.p)
+            return self.k
+        else:
+            return float('NaN')
     ######################################
 d=DH(90,2)
 #print(d.primes_gen())
-d.secret_number_generattor()
-
-print("prime number chosen: {}, number chosen 1<x<p-1: {}, g ^ x mod p:{}".format(d.p,d.a,d.y))
-d.secret_number_generattor()
-print("prime number chosen: {}, number chosen 1<x<p-1: {}, g ^ x mod p:{}".format(d.p,d.a,d.y))
+print("P= ", d.p)
+print("Generators: ",d.g_calculator())
+print("g= ",d.g, "** g is selected randomly among other numbers")
+print("Secret Key (K):",d.secret_number_generattor())
+print("Alice's private Key= ", d.a)
+print("Alice's Publuc Key= ", d.ya)
+print("Bob's private Key= ", d.b)
+print("Alice's Publuc Key= ", d.yb)
+#print("prime number chosen: {}, number chosen 1<x<p-1: {}, {} ^ ({}) mod {}:{}".format(d.p,d.a,d.g,d.a,d.p,d.ya))
+#print("prime number chosen: {}, number chosen 1<x<p-1: {}, {} ^ ({}) mod {}:{}".format(d.p,d.b,d.g,d.b,d.p,d.yb))
